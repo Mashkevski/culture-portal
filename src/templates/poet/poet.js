8b84eda68b46fccf1fddedbceb48325dd0c1a8b7
Siarhei Mashkeuski <cybera@list.ru>
2019-07-29 15:15:53 +0300

    feat: add dynamic author page

---
+++ src/templates/poet/poet.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import propTypes from 'prop-types';
+
+import Layout from '../../components/layout/layout';
+
+const Poet = ({ data }) => {
+  const { poet } = data.contentfulPoetDescription;
+  const { url } = data.contentfulPoetPicture.image.file;
+  return (
+    <Layout>
+      <h2>{poet.name}</h2>
+      <img alt={poet.name} src={url} />
+    </Layout>
+  );
+};
+
+export default Poet;
+
+export const query = graphql`
+  query($slug: String!) {
+    contentfulPoetDescription(title: {eq: $slug}) {
+      poet {
+        name
+        birthPlace
+        date
+        img
+        lng
+        videoId
+        vita
+        timelineData {
+          date
+          text
+        }
+        work {
+          date
+          title
+        }
+      }
+    }
+    contentfulPoetPicture(image: {title: {eq: $slug}}) {
+      image {
+        file {
+          url
+        }
+        title
+      }
+    }
+  }
+`;
+
+Poet.propTypes = {
+  data: propTypes.string,
+};
+
+Poet.defaultProps = {
+  data: '',
+};
