1647b1a411f599b3996e0d239619cf33107fd075
Maryna Petrenko <marinbonibon@gmail.com>
2019-07-31 23:17:38 +0300

    fix: implement localisation for button in author of the day component

--- src/components/authorOfTheDay/authorOfTheDay.js
+++ src/components/authorOfTheDay/authorOfTheDay.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, injectIntl, FormattedMessage } from 'gatsby-plugin-intl';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import { lightBlue } from '@material-ui/core/colors';
@@ -17,28 +17,30 @@ const ColorButton = withStyles(theme => ({
   },
 }))(Button);
 
-const AuthorOfTheDay = ({ poet, url }) => {
-  return (
-    <div className={styles.author}>
-      <h2 className={styles.author_title}>
+const AuthorOfTheDay = ({ poet, url }) => (
+  <div className={styles.author}>
+    <h2 className={styles.author_title}>
         Author of the day
-      </h2>
-      <h3 className={styles.author_name}>
-        {poet.name}
-      </h3>
-      <p className={styles.author_date}>
-        {poet.date}
-      </p>
-      <img className={styles.author_photo} src={url} alt="Author" />
-      <p className={styles.author_vita}>
-        {poet.vita}
-      </p>
-      <Link to="/author" className={styles.author_btn}><ColorButton variant="contained">Read more</ColorButton></Link>
-    </div>
-  );
-};
+    </h2>
+    <h3 className={styles.author_name}>
+      {poet.name}
+    </h3>
+    <p className={styles.author_date}>
+      {poet.date}
+    </p>
+    <img className={styles.author_photo} src={url} alt="Author" />
+    <p className={styles.author_vita}>
+      {poet.vita}
+    </p>
+    <Link to="/author" className={styles.author_btn}>
+      <ColorButton variant="contained">
+        <FormattedMessage id="button" />
+      </ColorButton>
+    </Link>
+  </div>
+);
 
-export default AuthorOfTheDay;
+export default injectIntl(AuthorOfTheDay);
 
 
 AuthorOfTheDay.propTypes = {
