bbe932cbaf7615a6ab440b49541254ecd5cff9ba
deminiro <43145879+deminiro@users.noreply.github.com>
2019-07-31 10:44:09 +0300

    refactor: fix merge conflict

--- src/templates/poet/poet.js
+++ src/templates/poet/poet.js
@@ -13,6 +13,10 @@ const Poet = ({ data }) => {
       <h2>{poet.name}</h2>
       <img alt={poet.name} src={url} />
       <Timeline poet={poet} />
+      <img alt={poet.name} src={image.file.url} />
+      <ul>
+        {images.map(img => <li><img src={img.file.url} alt={poet.name} /></li>)}
+      </ul>
     </Layout>
   );
 };
